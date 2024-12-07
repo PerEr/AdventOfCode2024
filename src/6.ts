c1d265c2a97ee465a4cd03a4cd527f8809ae27cf
Per Ersson <per.ersson@gmail.com>
2024-12-07 12:07:13 +0100

    Day6, p2

--- src/6.ts
+++ src/6.ts
@@ -1,37 +1,69 @@
 import { readFileSync } from "fs";
-const grid = readFileSync("data/6.txt", "utf8").split("\n");
 
-// find row, col for the character ^
-
-const start = grid
-    .map((line) => line.indexOf("^"))
-    .map((off, index) => off === -1 ? undefined : [index, off])
-    .find((v) => v) ?? [0,0];
-
-const directions = [[-1,0], [0,1], [1, 0], [0,-1]]
-let dirInxex = 0;
-
-console.log(start)
-
-// set of all visited positions as a tuple (row, col)
-const visited: Set<string> = new Set();
-
-let [row, col]: number[] = start;
-while(true) {
-    const cr = row + directions[dirInxex][0];
-    const cc = col + directions[dirInxex][1];
-    console.log(cr, cc);
-    if (cr < 0 || cr >= grid.length || cc < 0 || cc >= grid[0].length) {
-        break
-    }
-    if (grid[cr][cc] === "#") {
-        dirInxex = (dirInxex + 1) % 4;
-    } else {
-        const key = [cr, cc].join(",");
-        visited.add(key);
-        row = cr;
-        col = cc;
-    }
+interface Location {
+  row: number;
+  col: number;
 }
 
-console.log("Part1: ", visited.size);
+const DIRECTIONS = [
+  [-1, 0],
+  [0, 1],
+  [1, 0],
+  [0, -1],
+];
+
+const getKey = (row: number, col: number, dirIndex?: number) =>
+  dirIndex !== undefined ? `${row},${col},${dirIndex}` : `${row},${col}`;
+
+const { grid, startPos } = ((filename: string) => {
+  const grid = readFileSync(filename, "utf8").split("\n");
+  const start = grid.flatMap((line, row) =>
+    line.includes("^") ? [{ row, col: line.indexOf("^") }] : []
+  )[0] || { row: 0, col: 0 };
+  return { grid, startPos: start };
+})("data/6.txt");
+
+console.log(startPos);
+
+const traverse = (
+  grid: string[],
+  pos: Location,
+  extraPos?: Location
+): Set<string> | undefined => {
+  const visited: Set<string> = new Set();
+  let { row, col } = pos;
+  let dirIndex = 0;
+
+  while (true) {
+    const cr = row + DIRECTIONS[dirIndex][0];
+    const cc = col + DIRECTIONS[dirIndex][1];
+    if (cr < 0 || cr >= grid.length || cc < 0 || cc >= grid[0].length) break;
+
+    if (
+      grid[cr][cc] === "#" ||
+      (cr === extraPos?.row && cc === extraPos?.col)
+    ) {
+      dirIndex = (dirIndex + 1) % 4;
+    } else {
+      row = cr;
+      col = cc;
+      const key = getKey(row, col, extraPos ? dirIndex : undefined);
+      if (extraPos && visited.has(key)) return visited;
+      visited.add(key);
+    }
+  }
+  return extraPos ? undefined : visited;
+};
+
+console.log("Part1: ", traverse(grid, startPos)!.size);
+
+console.log(
+  "Part2: ",
+  [...traverse(grid, startPos)!].filter((pos) => {
+    const [row, col] = pos.split(",").map(Number);
+    return (
+      !(row === startPos.row && col === startPos.col) &&
+      traverse(grid, startPos, { row, col }) != undefined
+    );
+  }).length
+);
